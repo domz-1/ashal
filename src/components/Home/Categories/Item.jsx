c060fe87cdabc21add5af2add31a3212da2e1b6c
Adham Abo Elenin <adhamelembrator3@gmail.com>
2024-11-12 15:28:28 +0200

    Final Commit Insha2allah

--- src/components/Home/Categories/Item.jsx
+++ src/components/Home/Categories/Item.jsx
@@ -6,7 +6,9 @@ const Item = ({ img, storeName, productName, price, OldPrice }) => {
             </div>
             <div className="text">
                 <p className="store-name">{storeName}</p>
-                <h3 className="product-name">{productName}</h3>
+                <h3 className="product-name" style={{ cursor: 'pointer' }}>
+                    {productName}
+                </h3>
                 <div className="prices">
                     <span className="new-price">{price}</span>
                     <span className="old-price">{OldPrice}</span>
